79d61f3e09e9b72a453c15d3bfffc64cbc3d8360
Alexander Zhalnerchik <AM6734@corp.transmission-it.de>
2026-04-25 13:47:15 +0200

    Add reactive form

--- src/app/pages/login-page/login-page.component.ts
+++ src/app/pages/login-page/login-page.component.ts
@@ -1,15 +1,26 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login-page',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './login-page.component.html',
   styleUrl: './login-page.component.scss',
 })
 export class LoginPageComponent {
   showPassword = false;
 
+  form = new FormGroup({
+    username: new FormControl(null),
+    password: new FormControl(null),
+  });
+
   togglePasswordVisibility(): void {
     this.showPassword = !this.showPassword;
   }
+
+  onSubmit() {
+    console.log(this.form.value);
+  }
+
 }
